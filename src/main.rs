063ac9c05bab7769f0bf5a2cbf5eac68fd72972e
vivekvardhanadepu <vivekvicky839@gmail.com>
2022-07-22 04:22:26 +0530

    feat: takig 3 cmdline args functionality added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = args();
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    // let first_num: f32 = first.parse::<f32>().unwrap();
+    // let second_num: f32 = second.parse::<f32>().unwrap();
+    println!("{} {} {}", first, operator, second);
 }
