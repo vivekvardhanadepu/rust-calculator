2e897bd322e91c1e6f1e4031447f65ae9860f627
vivekvardhanadepu <vivekvicky839@gmail.com>
2022-07-23 00:26:44 +0530

    feat: DMAS operations using if else

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use std::env::{args, Args};
 
 fn main() {
-    let mut args = args();
+    let mut args: Args = args();
     let first = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
-    // let first_num: f32 = first.parse::<f32>().unwrap();
-    // let second_num: f32 = second.parse::<f32>().unwrap();
-    println!("{} {} {}", first, operator, second);
+    let first_num: f32 = first.parse::<f32>().unwrap();
+    let second_num: f32 = second.parse::<f32>().unwrap();
+
+    let result = operate(operator, first_num, second_num);
+    println!("{} {} {} is {}", first_num, operator, second_num, result);
+}
+
+fn operate(operator: char, a: f32, b:f32) -> f32 {
+    if operator == '+' {
+        a + b
+    } else if operator == '-' {
+        a - b
+    } else if operator == '*' {
+        a*b
+    } else if operator == '/' {
+        a/b
+    } else {
+        0.0
+    }
 }
